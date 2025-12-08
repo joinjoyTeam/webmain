d30f6e7c06eff7cc3b8555252fbec1e8459d3ca7
่joinjoytravel <joinjoytravel@gmail.com>
2025-12-08 19:45:27 +0700

    build for main/docs

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { fileURLToPath } from "url";
 
-//const __filename = fileURLToPath(import.meta.url);
-//const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
- base: "/",        // ✅ สำหรับ custom domain
+  base: "/reallocalforglobal/",
   build: {
-    outDir: "docs", // ✅ ให้ GitHub Pages ใช้ main/docs
+    outDir: "docs",
   },
 });
