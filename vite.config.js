b92cdc94da2d5ca7925b6acb7098a484330cb413
่joinjoytravel <joinjoytravel@gmail.com>
2025-12-08 20:04:53 +0700

    update website

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
-  base: "/reallocalforglobal/",
+  base: "/main/",
   build: {
     outDir: "docs",
   },
